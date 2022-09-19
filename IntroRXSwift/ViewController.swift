2c1814dce37c03154e19c611122286c31d966485
Alexey Kulish <alexkulish1993@gmail.com>
2022-09-19 20:25:21 +0300

    Introduction in RxSwift

--- IntroRXSwift/ViewController.swift
+++ IntroRXSwift/ViewController.swift
@@ -6,14 +6,64 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
+struct Item {
+    let imageString: String
+    let title: String
+}
+
+struct ItemViewModel {
+    var items = PublishSubject<[Item]>()
+    
+    func fetchItems() {
+        let products = [
+        Item(imageString: "house", title: "Home"),
+        Item(imageString: "gear", title: "Settings"),
+        Item(imageString: "person.circle", title: "Profile"),
+        Item(imageString: "airplane", title: "Flights"),
+        Item(imageString: "bell", title: "Activity"),
+        ]
+        
+        items.onNext(products)
+        items.onCompleted()
+    }
+}
 
 class ViewController: UIViewController {
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    private var viewModel = ItemViewModel()
+    private var bag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        bindTableData()
     }
-
+    
+    func bindTableData() {
+        
+        // Bind items to table
+        viewModel.items.bind(to: tableView.rx.items( cellIdentifier: "cell", cellType: UITableViewCell.self)) { row, model, cell in
+            cell.textLabel?.text = model.title
+            cell.imageView?.image = UIImage(systemName: model.imageString)
+        }.disposed(by: bag)
+        // Bind a model selected handler
+        tableView.rx.modelSelected(Item.self).bind { item in
+            print(item.title)
+        }.disposed(by: bag)
+        // fetch items
+        viewModel.fetchItems()
+    }
+    
 
 }
 
